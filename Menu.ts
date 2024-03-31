0b41042079ddc2280c55da96c6b120a93db9a73f
hunterblp <milena.sforte@gmail.com>
2024-03-31 19:56:11 -0300

    Atualizando cores menu

--- Menu.ts
+++ Menu.ts
@@ -1,72 +1,85 @@
 import readline = require("readline-sync");
+import { colors } from "./src/util/Colors";
 
-let opcao: number, continuar_loop: boolean = true;
+export function main() {
 
-while (continuar_loop) {
+    let opcao: number;
 
-    console.log("\n******************************************************");
-    console.log("*                                                    *");
-    console.log("*                  BANCO DA MILENA                   *");
-    console.log("*                                                    *");
-    console.log("******************************************************");
-    console.log("*                                                    *");
-    console.log("*         1. Criar Conta                             *");
-    console.log("*         2. Listar todas as Contas                  *");
-    console.log("*         3. Buscar Conta por Numero                 *");
-    console.log("*         4. Atualizar Dados da Conta                *");
-    console.log("*         5. Apagar conta                            *");
-    console.log("*         6. Sacar                                   *");
-    console.log("*         7. Depositar                               *");
-    console.log("*         8. Transferir valores entre Contas         *");
-    console.log("*         9. Sair                                    *");
-    console.log("*                                                    *");
-    console.log("******************************************************");
-    console.log("                                                      ");
+    while (true) {
 
-    opcao = readline.questionInt("Digite o numero da opcao desejada: ");
+        console.log(colors.fg.green, "\n******************************************************");
+        console.log("*                                                    *");
+        console.log("*                  BANCO DA MILENA                   *");
+        console.log("*                                                    *");
+        console.log("******************************************************");
+        console.log("*                                                    *");
+        console.log("*         1. Criar Conta                             *");
+        console.log("*         2. Listar todas as Contas                  *");
+        console.log("*         3. Buscar Conta por Numero                 *");
+        console.log("*         4. Atualizar Dados da Conta                *");
+        console.log("*         5. Apagar conta                            *");
+        console.log("*         6. Sacar                                   *");
+        console.log("*         7. Depositar                               *");
+        console.log("*         8. Transferir valores entre Contas         *");
+        console.log("*         9. Sair                                    *");
+        console.log("*                                                    *");
+        console.log("******************************************************");
+        console.log("                                                      ", colors.reset);
 
-    switch (opcao) {
+        opcao = readline.questionInt("Digite o numero da opcao desejada: ");
 
-        case 1:
-            console.log("Voce escolheu: Criar conta");
-            break;
+        if (opcao == 9) {
+            console.log("\nBanco da Milena - O seu futuro começa aqui!");
+            sobre();
+        }
 
-        case 2:
-            console.log("Voce escolheu: Listar todas as Contas");
-            break;
+        switch (opcao) {
 
-        case 3:
-            console.log("Voce escolheu: Buscar Conta por Numero");
-            break;
+            case 1:
+                console.log("\n\nVoce escolheu: Criar conta\n\n");
+                break;
 
-        case 4:
-            console.log("Voce escolheu: Atualizar Dados da Conta");
-            break;
+            case 2:
+                console.log("\n\nVoce escolheu: Listar todas as Contas\n\n");
+                break;
 
-        case 5:
-            console.log("Voce escolheu: Apagar conta ");
-            break;
+            case 3:
+                console.log("\n\nVoce escolheu: Buscar Conta por Numero\n\n");
+                break;
 
-        case 6:
-            console.log("Voce escolheu: Sacar ");
-            break;
+            case 4:
+                console.log("\n\nVoce escolheu: Atualizar Dados da Conta\n\n");
+                break;
 
-        case 7:
-            console.log("Voce escolheu: Depositar");
-            break;
+            case 5:
+                console.log("\n\nVoce escolheu: Apagar conta\n\n");
+                break;
 
-        case 8:
-            console.log("Voce escolheu: Transferir valores entre Contas");
-            break;
+            case 6:
+                console.log("\n\nVoce escolheu: Sacar\n\n");
+                break;
 
-        case 9:
-            continuar_loop = false;
-            break;
+            case 7:
+                console.log("\n\nVoce escolheu: Depositar\n\n");
+                break;
 
-        default:
-            console.log("Opção inválida!")
+            case 8:
+                console.log("\n\nVoce escolheu: Transferir valores entre Contas\n\n");
+                break;
 
+            default:
+                console.log("Opção inválida!");
+                break;
+
+        }
     }
+
 }
 
-console.log("Volte sempre!")
+export function sobre(): void {
+    console.log("\n__________________________________________________");
+    console.log("|   Projeto Desenvolvido por: Milena Semanaka    |");
+    console.log("__________________________________________________");
+}
+
+main()
